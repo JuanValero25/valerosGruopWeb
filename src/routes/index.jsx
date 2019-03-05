15fca898f7fd2cde64ef61a87e2f789052dc6434
skateboardman9@hotmail.com <cpx5842698>
2019-03-05 12:49:43 -0300

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,7 @@
+import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
+
+const indexRoutes = [
+  { path: "/", name: "ProfilePage", component: ProfilePage }
+];
+
+export default indexRoutes;
---
+++ src/views/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,226 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+import Camera from "@material-ui/icons/Camera";
+import Palette from "@material-ui/icons/Palette";
+import Favorite from "@material-ui/icons/Favorite";
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import NavPills from "components/NavPills/NavPills.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import profile from "assets/img/logo.svg";
+
+import studio1 from "assets/img/examples/studio-1.jpg";
+import studio2 from "assets/img/examples/studio-2.jpg";
+import studio3 from "assets/img/examples/studio-3.jpg";
+import studio4 from "assets/img/examples/studio-4.jpg";
+import studio5 from "assets/img/examples/studio-5.jpg";
+import work1 from "assets/img/examples/olu-eletu.jpg";
+import work2 from "assets/img/examples/clem-onojeghuo.jpg";
+import work3 from "assets/img/examples/cynthia-del-rio.jpg";
+import work4 from "assets/img/examples/mariya-georgieva.jpg";
+import work5 from "assets/img/examples/clem-onojegaw.jpg";
+
+import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx";
+
+class ProfilePage extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    const imageClasses = classNames(
+      classes.imgRaised,
+      classes.imgRoundedCircle,
+      classes.imgFluid
+    );
+    const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+    return (
+      <div>
+        <Header
+          color="transparent"
+          brand="Valeros Group"
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 200,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax image={ require("assets/img/backg.webp")} />
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <div>
+            <div className={classes.container}>
+              <GridContainer justify="center">
+                <GridItem xs={12} sm={12} md={6}>
+                  <div className={classes.profile}>
+                    <div>
+                      <img src={profile} alt="..." className={imageClasses} />
+                    </div>
+                    <div className={classes.name}>
+                      <h3 className={classes.title}>Jose Valero</h3>
+                      <h6>Jefe Ejecutivo</h6>
+                    </div>
+                  </div>
+                </GridItem>
+              </GridContainer>
+              <div className={classes.description}>
+                <p>
+                  Somos un equipo de profesionales en el área Marketing,
+                  desarrollo web y comunicación social, curiosos creativos y
+                  apasionados de las estrategias digitales, que disfrutan de lo
+                  que mejor saben hacer, planificar, crear y comunicar. Nuestro
+                  equipo se especializa en desarrollar estrategias, identificar
+                  insights y analizar datos. Potenciamos tu marca y anticipamos
+                  las necesidades de la audiencia. Optimizando la performance de
+                  inversiones y recursos.
+                </p>
+              </div>
+              <GridContainer justify="center">
+                <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+                  <NavPills
+                    alignCenter
+                    color="primary"
+                    tabs={[
+                      {
+                        tabButton: "Studio",
+                        tabIcon: Camera,
+                        tabContent: (
+                          <GridContainer justify="center">
+                            <GridItem xs={12} sm={12} md={4}>
+                              <img
+                                alt="..."
+                                src={studio1}
+                                className={navImageClasses}
+                              />
+                              <img
+                                alt="..."
+                                src={studio2}
+                                className={navImageClasses}
+                              />
+                            </GridItem>
+                            <GridItem xs={12} sm={12} md={4}>
+                              <img
+                                alt="..."
+                                src={studio5}
+                                className={navImageClasses}
+                              />
+                              <img
+                                alt="..."
+                                src={studio4}
+                                className={navImageClasses}
+                              />
+                            </GridItem>
+                          </GridContainer>
+                        )
+                      },
+                      {
+                        tabButton: "Work",
+                        tabIcon: Palette,
+                        tabContent: (
+                          <GridContainer justify="center">
+                            <GridItem xs={12} sm={12} md={4}>
+                              <img
+                                alt="..."
+                                src={work1}
+                                className={navImageClasses}
+                              />
+                              <img
+                                alt="..."
+                                src={work2}
+                                className={navImageClasses}
+                              />
+                              <img
+                                alt="..."
+                                src={work3}
+                                className={navImageClasses}
+                              />
+                            </GridItem>
+                            <GridItem xs={12} sm={12} md={4}>
+                              <img
+                                alt="..."
+                                src={work4}
+                                className={navImageClasses}
+                              />
+                              <img
+                                alt="..."
+                                src={work5}
+                                className={navImageClasses}
+                              />
+                            </GridItem>
+                          </GridContainer>
+                        )
+                      },
+                      {
+                        tabButton: "Favorite",
+                        tabIcon: Favorite,
+                        tabContent: (
+                          <GridContainer justify="center">
+                            <GridItem xs={12} sm={12} md={4}>
+                              <img
+                                alt="..."
+                                src={work4}
+                                className={navImageClasses}
+                              />
+                              <img
+                                alt="..."
+                                src={studio3}
+                                className={navImageClasses}
+                              />
+                            </GridItem>
+                            <GridItem xs={12} sm={12} md={4}>
+                              <img
+                                alt="..."
+                                src={work2}
+                                className={navImageClasses}
+                              />
+                              <img
+                                alt="..."
+                                src={work1}
+                                className={navImageClasses}
+                              />
+                              <img
+                                alt="..."
+                                src={studio1}
+                                className={navImageClasses}
+                              />
+                            </GridItem>
+                          </GridContainer>
+                        )
+                      }
+                    ]}
+                  />
+                </GridItem>
+              </GridContainer>
+            </div>
+            <div className={classes.center}>
+              <h4 className={classes.description}>
+                escribenos por whatsapp!
+              </h4>
+              <div align="center">
+                <Button
+                  href="https://api.whatsapp.com/send?phone=+5491132386342&text= i am interest on your development services"
+                  className={classes.center}
+                  color="success"
+                >
+                  Send whatsapp message
+                </Button>
+              </div>
+              <br />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withStyles(profilePageStyle)(ProfilePage);
